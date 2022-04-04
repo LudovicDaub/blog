425df98bc6736a251719b2d5ae86f1abb6c024c3
unknown <ludovicdaub@sfr.fr>
2022-04-04 16:05:04 +0200

    first commit

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i < 10; $i++) {
+            $article = new Article();
+        }
+
+        $manager->flush();
+    }
+}
